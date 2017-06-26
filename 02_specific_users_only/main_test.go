fefec966fc249f1b829aef88a146c780b95348a8
Phillip Dudley <Predatorian3@gmail.com>
2017-06-25 20:12:54 -0500

    Added Rakefiles to help with multi-step builds.

--- 02_specific_users_only/main_test.go
+++ 02_specific_users_only/main_test.go
@@ -1,23 +1,23 @@
 package specificPerson
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestSayHelloToSpecificPerson(t *testing.T) {
-    var name_list = []struct{
-        input string
-        expected string
-    }{
-        {"Phillip", "Hello Phillip"},
-        {"Phil", "Hello Phil"},
-        {"David", "You're not authorized to use this program."},
-    }
-    
-    for _, tt := range name_list {
-        result := SayHelloToSpecificPerson(tt.input)
-        if result != tt.expected {
-            t.Errorf("SayHelloToSpecificPerson() took %s, expected %s, but instead got %s", tt.input, tt.expected, result)
-        }
-    }
+	var name_list = []struct {
+		input    string
+		expected string
+	}{
+		{"Phillip", "Hello Phillip"},
+		{"Phil", "Hello Phil"},
+		{"David", "You're not authorized to use this program."},
+	}
+
+	for _, tt := range name_list {
+		result := SayHelloToSpecificPerson(tt.input)
+		if result != tt.expected {
+			t.Errorf("SayHelloToSpecificPerson() took %s, expected %s, but instead got %s", tt.input, tt.expected, result)
+		}
+	}
 }
